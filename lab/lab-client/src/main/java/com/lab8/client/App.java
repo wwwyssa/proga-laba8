6814e3a8aae4a73a5e22794f117d4e05a3fc080b
wwwyssa <grisha.maryin@gmail.com>
2025-06-08 15:04:15 +0300

    upd

---
+++ lab/lab-client/src/main/java/com/lab8/client/App.java
@@ -0,0 +1,13 @@
+package com.lab8.client;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
